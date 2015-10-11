0b3597ffb9f79ca8e1c93f6a6159cc71c4067075
Chris <bahrango@hotmail.com>
2015-10-11 12:51:36 -0600

    use pushstate

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 export class App {
   configureRouter(config, router){
     config.title = 'Aurelia';
+    config.options.pushState = true;
     config.map([
       { route: ['','profile'],  name: 'profile',      moduleId: 'profile',      nav: true, title:'Profile' },
       { route: 'about',         name: 'about',        moduleId: 'about',        nav: true, title:'About' },
