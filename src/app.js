42e04874565458d7c69df70178df880a712b493a
Chris <bahrango@hotmail.com>
2015-10-21 17:37:41 -0600

    add thumbs to projects view

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 export class App {
   configureRouter(config, router){
-    config.title = 'Aurelia';
+    config.title = 'LADR';
     config.options.pushState = true;
     config.map([
-      { route: ['','profile'],                  name: 'profile',      moduleId: 'profile',      nav: true, title:'Profile' },
-      { route: 'about',                         name: 'about',        moduleId: 'about',        nav: true, title:'About' },
-      { route: 'awards',                        name: 'awards',       moduleId: 'awards',       nav: true, title:'Awards' },
-      { route: 'projects',  name: 'projects',     moduleId: 'projects',     nav: true, title:'Projects' },
-      { route: 'contact',                       name: 'contact',      moduleId: 'contact',      nav: true, title:'Contact' }
+      { route: ['','profile'],  name: 'profile',      moduleId: 'profile',      nav: true, title:'Profile' },
+      { route: 'about',         name: 'about',        moduleId: 'about',        nav: true, title:'About' },
+      { route: 'awards',        name: 'awards',       moduleId: 'awards',       nav: true, title:'Awards' },
+      { route: 'projects',      name: 'projects',     moduleId: 'projects',     nav: true, title:'Projects' },
+      { route: 'contact',       name: 'contact',      moduleId: 'contact',      nav: true, title:'Contact' }
     ]);
 
     this.router = router;
--- src/projects.js
+++ src/projects.js
@@ -3,34 +3,39 @@ import {HttpClient} from 'aurelia-fetch-client';
 import {Router} from 'aurelia-router';
 import {DialogService} from 'aurelia-dialog';
 import {Project} from './project';
+import {Base} from './base';
 import 'fetch';
 
 @inject(HttpClient, Router, DialogService)
 export class Projects {
-  projects = [];
-  allProjects = [];
-  categories = [];
-  query = "";
-  openProject = false;
+    projects = [];
+    allProjects = [];
+    categories = [];
+    query = "";
 
-  constructor(http, router, dialogService){
-    http.configure(config => {
-      config
-        .useStandardConfiguration();
-    });
+    constructor(http, router, dialogService) {
+        http.configure(config => {
+          config
+            .useStandardConfiguration();
+        });
 
-    this.http = http;
-    this.dialogService = dialogService;
-  }
+        this.http = http;
+        this.dialogService = dialogService;
+    }
 
-  changeCat(cat) {
-      cat = cat.toLowerCase();
-      this.projects = this.allProjects.filter(function(p) {
-          return p.categories.some(function(c) { return c.toLowerCase().indexOf(cat) !== -1 });
-      });
-  }
+    changeCat(cat) {
+        if (!cat) {
+            this.projects = this.allProjects;
+            return;
+        }
 
-  search(q) {
+        cat = cat.toLowerCase();
+        this.projects = this.allProjects.filter(function(p) {
+            return p.categories.some(function(c) { return c.toLowerCase().indexOf(cat) !== -1 });
+        });
+    }
+
+    search(q) {
       this.query = q || this.query;
       var q = this.query.toLowerCase();
       this.projects = this.allProjects.filter(function(p) {
@@ -38,7 +43,7 @@ export class Projects {
                  p.tags.some(function(t) { return t.toLowerCase().indexOf(q) !== -1 }) ||
                  p.name.toLowerCase().indexOf(q) !== -1;
       });
-  }
+    }
     project(slug) {
         var project = this.allProjects.find(function(p) {
             return p.slug && p.slug.toLowerCase() === slug.toLowerCase();
@@ -55,12 +60,13 @@ export class Projects {
             .then(projects => {
                 this.allProjects = this.projects = projects;
                 this.categories = projects.reduce(function(l, c) {
-                    return l.concat(c.categories.map(function(e) { return { key: e.toLowerCase(), value: e }; }));
+                        return l.concat(c.categories.map(function(e) { return { key: e.toLowerCase(), value: e }; }));
                     }, [])
                     .unique()
                     .sort(function(a,b) {
-                    return a.localeCompare(b);
-                });
+                        return a.localeCompare(b);
+                    });
+                    console.log(this.categories);
 
                 if (params.cat) {
                     this.changeCat(params.cat);
